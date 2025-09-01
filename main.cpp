8bc2ad6f9d52a815ad41a31e980333cef355352a
zhouxuguang <zhouxuguang236@126.com>
2025-09-01 17:10:58 +0800

    强制独显运行

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "MainWindow.h"
 #include <QApplication>
 
+#ifdef _WIN32
+#include <Windows.h>
+
+extern "C" __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+
+extern "C" __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 0x00000001;
+#endif // _WIN32
+
 int main(int argc, char *argv[]){
     QApplication app(argc, argv);
 
